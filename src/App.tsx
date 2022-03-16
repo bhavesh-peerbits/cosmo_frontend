4b9a2a2fa5cfd602ad5d2c6ca4df399ba616ab19
Stefano Imperiale <stefano.imperiale@aizoongroup.com>
2022-03-16 15:09:10 +0100

    Fix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,12 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-
+  console.log('CIAO');
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
-
         <p>
           <button type="button" onClick={() => setCount(c => c + 1)}>
             count is: {count}
