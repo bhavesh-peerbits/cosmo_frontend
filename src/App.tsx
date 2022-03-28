f0f0a22e0c7b2d506282f0f82968079488807c00
Stefano Imperiale <stefano.imperiale@aizoongroup.com>
2022-03-28 14:55:43 +0200

    Fix side navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 import {
+	Notification,
+	Search,
+	Switcher as SwitcherIcon,
+	Fade
+} from '@carbon/react/icons';
+import {
 	Header,
 	Theme,
 	HeaderMenu,
@@ -19,23 +25,17 @@ import {
 	Switcher,
 	SwitcherItem,
 	SwitcherDivider,
-	Content
+	Content,
+	SkipToContent
 } from '@carbon/react';
-import {
-	Notification,
-	Search,
-	Switcher as SwitcherIcon,
-	Fade
-} from '@carbon/react/icons';
 import 'style/app.scss';
 import useGetExample from '@api/useGetExample';
 import ErrorBoundary from '@error/components/ErrorBoundary';
 import { Suspense, useEffect, useState } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 
 const Test = () => {
 	const { data } = useGetExample();
-	// eslint-disable-next-line no-console
-	console.log(data);
 	const [error, setError] = useState(false);
 	setTimeout(() => {
 		setError(true);
@@ -48,77 +48,95 @@ const Test = () => {
 	return <div>{JSON.stringify(data)}</div>;
 };
 
-const App = () => (
-	<Theme theme='g100' className='h-full'>
-		<HeaderContainer
-			render={({ isSideNavExpanded, onClickSideNavExpand }) => (
-				<>
-					<SideNav
-						aria-label='Side navigation'
-						expanded={isSideNavExpanded}
-						isChildOfHeader={false}
+const StoryContent = () => {
+	const content = (
+		<div className='bx--grid'>
+			<div className='bx--row'>
+				<section className='bx--offset-lg-3 bx--col-lg-13'>
+					<h2
+						style={{
+							fontWeight: '800',
+							margin: '30px 0',
+							fontSize: '20px'
+						}}
 					>
-						<SideNavItems>
-							<HeaderSideNavItems hasDivider>
-								<HeaderMenuItem href='#'>Link 1</HeaderMenuItem>
-								<HeaderMenuItem href='#'>Link 2</HeaderMenuItem>
-								<HeaderMenuItem href='#'>Link 3</HeaderMenuItem>
-								<HeaderMenu aria-label='Link 4' menuLinkName='Link 4'>
-									<HeaderMenuItem href='#'>Sub-link 1</HeaderMenuItem>
-									<HeaderMenuItem href='#'>Sub-link 2</HeaderMenuItem>
-									<HeaderMenuItem href='#'>Sub-link 3</HeaderMenuItem>
-								</HeaderMenu>
-							</HeaderSideNavItems>
-							<SideNavMenu renderIcon={Fade} title='Category title'>
-								<SideNavMenuItem href='https://www.carbondesignsystem.com/'>
-									Link
-								</SideNavMenuItem>
-								<SideNavMenuItem href='https://www.carbondesignsystem.com/'>
-									Link
-								</SideNavMenuItem>
-								<SideNavMenuItem href='https://www.carbondesignsystem.com/'>
-									Link
-								</SideNavMenuItem>
-							</SideNavMenu>
-							<SideNavMenu renderIcon={Fade} title='Category title'>
-								<SideNavMenuItem href='https://www.carbondesignsystem.com/'>
-									Link
-								</SideNavMenuItem>
-								<SideNavMenuItem href='https://www.carbondesignsystem.com/'>
-									Link
-								</SideNavMenuItem>
-								<SideNavMenuItem href='https://www.carbondesignsystem.com/'>
-									Link
-								</SideNavMenuItem>
-							</SideNavMenu>
-							<SideNavMenu renderIcon={Fade} title='Category title' isActive>
-								<SideNavMenuItem href='https://www.carbondesignsystem.com/'>
-									Link
-								</SideNavMenuItem>
-								<SideNavMenuItem
-									aria-current='page'
-									href='https://www.carbondesignsystem.com/'
-								>
-									Link
-								</SideNavMenuItem>
-								<SideNavMenuItem href='https://www.carbondesignsystem.com/'>
-									Link
-								</SideNavMenuItem>
-							</SideNavMenu>
-							<SideNavLink renderIcon={Fade} href='https://www.carbondesignsystem.com/'>
-								Link
-							</SideNavLink>
-							<SideNavLink renderIcon={Fade} href='https://www.carbondesignsystem.com/'>
-								Link
-							</SideNavLink>
-						</SideNavItems>
-					</SideNav>
+						Purpose and function
+					</h2>
+					<p style={{ lineHeight: '20px' }}>
+						The shell is perhaps the most crucial piece of any UI built with Carbon. It
+						contains the shared navigation framework for the entire design system and ties
+						the products in IBM’s portfolio together in a cohesive and elegant way. The
+						shell is the home of the topmost navigation, where users can quickly and
+						dependably gain their bearings and move between pages.
+						<br />
+						<br />
+						The shell was designed with maximum flexibility built in, to serve the needs
+						of a broad range of products and users. Adopting the shell ensures compliance
+						with IBM design standards, simplifies development efforts, and provides great
+						user experiences. All IBM products built with Carbon are required to use the
+						shell’s header.
+						<br />
+						<br />
+						To better understand the purpose and function of the UI shell, consider the
+						“shell” of MacOS, which contains the Apple menu, top-level navigation, and
+						universal, OS-level controls at the top of the screen, as well as a universal
+						dock along the bottom or side of the screen. The Carbon UI shell is roughly
+						analogous in function to these parts of the Mac UI. For example, the app
+						switcher portion of the shell can be compared to the dock in MacOS.
+					</p>
+					<h2
+						style={{
+							fontWeight: '800',
+							margin: '30px 0',
+							fontSize: '20px'
+						}}
+					>
+						Header responsive behavior
+					</h2>
+					<p style={{ lineHeight: '20px' }}>
+						As a header scales down to fit smaller screen sizes, headers with persistent
+						side nav menus should have the side nav collapse into “hamburger” menu. See
+						the example to better understand responsive behavior of the header.
+					</p>
+					<h2
+						style={{
+							fontWeight: '800',
+							margin: '30px 0',
+							fontSize: '20px'
+						}}
+					>
+						Secondary navigation
+					</h2>
+					<p style={{ lineHeight: '20px' }}>
+						The side-nav contains secondary navigation and fits below the header. It can
+						be configured to be either fixed-width or flexible, with only one level of
+						nested items allowed. Both links and category lists can be used in the
+						side-nav and may be mixed together. There are several configurations of the
+						side-nav, but only one configuration should be used per product section. If
+						tabs are needed on a page when using a side-nav, then the tabs are secondary
+						in hierarchy to the side-nav.
+					</p>
+				</section>
+			</div>
+		</div>
+	);
+
+	return <Content>{content}</Content>;
+};
+
+const App = () => {
+	const [appExpanded, setAppExpanded] = useState(false);
+
+	return (
+		<Theme theme='g100' className='h-full overflow-hidden'>
+			<HeaderContainer
+				render={({ isSideNavExpanded, onClickSideNavExpand }) => (
 					<Header aria-label='IBM Platform Name'>
+						<SkipToContent />
 						<HeaderMenuButton
 							aria-label='Open menu'
 							onClick={onClickSideNavExpand}
 							isActive={isSideNavExpanded}
-							isCollapsible
 						/>
 						<HeaderName href='#' prefix='IBM'>
 							[Platform]
@@ -133,6 +151,7 @@ const App = () => (
 								<HeaderMenuItem href='#three'>Sub-link 3</HeaderMenuItem>
 							</HeaderMenu>
 						</HeaderNavigation>
+
 						<HeaderGlobalBar>
 							<HeaderGlobalAction aria-label='Search' onClick={() => {}}>
 								<Search />
@@ -142,15 +161,71 @@ const App = () => (
 							</HeaderGlobalAction>
 							<HeaderGlobalAction
 								aria-label='App Switcher'
-								isActive
-								onClick={() => {}}
+								isActive={appExpanded}
+								onClick={() => setAppExpanded(v => !v)}
 								tooltipAlignment='end'
 							>
 								<SwitcherIcon />
 							</HeaderGlobalAction>
 						</HeaderGlobalBar>
+						<SideNav aria-label='Side navigation' expanded={isSideNavExpanded}>
+							<SideNavItems>
+								<HeaderSideNavItems hasDivider>
+									<HeaderMenuItem href='#'>Link 1</HeaderMenuItem>
+									<HeaderMenuItem href='#'>Link 2</HeaderMenuItem>
+									<HeaderMenuItem href='#'>Link 3</HeaderMenuItem>
+									<HeaderMenu aria-label='Link 4' menuLinkName='Link 4'>
+										<HeaderMenuItem href='#'>Sub-link 1</HeaderMenuItem>
+										<HeaderMenuItem href='#'>Sub-link 2</HeaderMenuItem>
+										<HeaderMenuItem href='#'>Sub-link 3</HeaderMenuItem>
+									</HeaderMenu>
+								</HeaderSideNavItems>
+								<SideNavMenu renderIcon={Fade} title='Category title'>
+									<SideNavMenuItem href='https://www.carbondesignsystem.com/'>
+										Link
+									</SideNavMenuItem>
+									<SideNavMenuItem href='https://www.carbondesignsystem.com/'>
+										Link
+									</SideNavMenuItem>
+									<SideNavMenuItem href='https://www.carbondesignsystem.com/'>
+										Link
+									</SideNavMenuItem>
+								</SideNavMenu>
+								<SideNavMenu renderIcon={Fade} title='Category title'>
+									<SideNavMenuItem href='https://www.carbondesignsystem.com/'>
+										Link
+									</SideNavMenuItem>
+									<SideNavMenuItem href='https://www.carbondesignsystem.com/'>
+										Link
+									</SideNavMenuItem>
+									<SideNavMenuItem href='https://www.carbondesignsystem.com/'>
+										Link
+									</SideNavMenuItem>
+								</SideNavMenu>
+								<SideNavMenu renderIcon={Fade} title='Category title' isActive>
+									<SideNavMenuItem href='https://www.carbondesignsystem.com/'>
+										Link
+									</SideNavMenuItem>
+									<SideNavMenuItem
+										aria-current='page'
+										href='https://www.carbondesignsystem.com/'
+									>
+										Link
+									</SideNavMenuItem>
+									<SideNavMenuItem href='https://www.carbondesignsystem.com/'>
+										Link
+									</SideNavMenuItem>
+								</SideNavMenu>
+								<SideNavLink renderIcon={Fade} href='https://www.carbondesignsystem.com/'>
+									Link
+								</SideNavLink>
+								<SideNavLink renderIcon={Fade} element={Link} to='/test'>
+									Link
+								</SideNavLink>
+							</SideNavItems>
+						</SideNav>
 
-						<HeaderPanel aria-label='Header Panel' expanded>
+						<HeaderPanel aria-label='Header Panel' expanded={appExpanded}>
 							<Switcher aria-label='Switcher Container'>
 								<SwitcherItem isSelected aria-label='Link 1' href='#'>
 									Link 1
@@ -175,17 +250,26 @@ const App = () => (
 							</Switcher>
 						</HeaderPanel>
 					</Header>
-					<Content className='h-full w-full'>
-						<ErrorBoundary>
-							<Suspense fallback='LOADING'>
-								<Test />
-							</Suspense>
-						</ErrorBoundary>
-					</Content>
-				</>
-			)}
-		/>
-	</Theme>
-);
+				)}
+			/>
+			<Content className='container-w-sidenav h-full overflow-auto'>
+				<ErrorBoundary>
+					<Routes>
+						<Route index element={<StoryContent />} />
+						<Route
+							path='/test'
+							element={
+								<Suspense fallback='LOADING'>
+									<Test />
+								</Suspense>
+							}
+						/>
+						<Route path='*' element={<div>Not found</div>} />
+					</Routes>
+				</ErrorBoundary>
+			</Content>
+		</Theme>
+	);
+};
 
 export default App;
--- src/types/carbon__react/index.d.ts
+++ src/types/carbon__react/index.d.ts
@@ -1,9 +1,57 @@
 import '@carbon/react';
-import React from 'react';
+import React, { InputHTMLAttributes } from 'react';
+import { LinkProps } from 'react-router-dom';
 
 declare module '@carbon/react' {
 	const Theme: React.FC<{ theme: 'white' | 'g10' | 'g90' | 'g100'; className: string }>;
 	const Layer: React.FC;
+	const Header: React.FC;
+	const HeaderMenu: React.FC<{ 'aria-label'?: string; menuLinkName: string }>;
+	const Header: React.FC;
+	const HeaderContainer: React.FC<{
+		render: (props: {
+			isSideNavExpanded: boolean;
+			onClickSideNavExpand: () => void;
+		}) => JSX.Element;
+	}>;
+	const HeaderMenuButton: React.FC<{
+		'aria-label'?: string;
+		onClick?: () => void;
+		isActive: boolean;
+	}>;
+	const HeaderName: React.FC<{ prefix?: string; href?: string }>;
+	const HeaderNavigation: React.FC<{ 'aria-label'?: string }>;
+	const HeaderMenuItem: React.FC<{ href?: string }>;
+	const HeaderGlobalBar: React.FC;
+	const HeaderGlobalAction: React.FC<{
+		'aria-label'?: string;
+		onClick: () => void;
+		isActive?: boolean;
+		tooltipAlignment?: 'start' | 'center' | 'end';
+	}>;
+	const SideNav: React.FC<{ 'aria-label'?: string; expanded: boolean }>;
+	const SideNavItems: React.FC;
+	const HeaderSideNavItems: React.FC<{ hasDivider: boolean }>;
+	const SideNavMenu: React.FC<{
+		isActive?: boolean;
+		renderIcon: JSX.Element;
+		title: string;
+	}>;
+	const SideNavMenuItem: React.FC<{ href?: string }>;
+	const SideNavLink: React.FC<{
+		renderIcon?: JSX.Element;
+		element?: React.ForwardRefExoticComponent<
+			LinkProps & React.RefAttributes<HTMLAnchorElement>
+		>;
+		href?: string;
+		to?: string;
+	}>;
+	const HeaderPanel: React.FC<{ expanded: boolean }>;
+	const Switcher: React.FC;
+	const SwitcherItem: React.FC<{ isSelected?: boolean; href?: string }>;
+	const SwitcherDivider: React.FC;
+	const Content: React.FC<{ className?: string }>;
+	const SkipToContent: React.FC;
 	const ActionableNotification: React.FC<{
 		onActionButtonClick?: () => void;
 		inline?: boolean;
@@ -11,4 +59,35 @@ declare module '@carbon/react' {
 		kind?: 'error' | 'info' | 'warning';
 		className?: string;
 	}>;
+	const Accordion: React.FC;
+	const AccordionItem: React.FC<{ title: string }>;
+	const Button: React.FC<React.HTMLProps<HTMLButtonElement>>;
+	const CodeSnippet: React.FC<{ type: 'inline' | 'multi' }>;
+	const ComposedModal: React.FC<{ open?: boolean; preventCloseOnClickOutside?: boolean }>;
+	const InlineLoading: React.FC<{
+		status?: 'active';
+		iconDescription?: string;
+		description?: string;
+	}>;
+	const Layer: React.FC;
+	const ModalBody: React.FC;
+	const ModalFooter: React.FC;
+	const ModalHeader: React.FC<{ title: string; label: string; closeModal: () => void }>;
+	const TextArea: React.FC<
+		InputHTMLAttributes<HTMLTextAreaElement> & {
+			invalid?: boolean;
+			invalidText?: string;
+			labelText?: string;
+			helperText?: string;
+			cols?: number;
+			rows?: number;
+		}
+	>;
+	const TextInput: React.FC<
+		InputHTMLAttributes<HTMLInputElement> & {
+			invalid?: boolean;
+			invalidText?: string;
+			labelText?: string;
+		}
+	>;
 }
