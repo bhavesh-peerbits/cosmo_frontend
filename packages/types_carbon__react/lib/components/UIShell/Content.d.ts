c35f46369b5b055cce6db6a2d8ed6c2d8f7fbfc1
Stefano Imperiale <stefano.imperiale@aizoongroup.com>
2022-04-06 14:57:04 +0200

    Fix Language Prompt style + add definition

--- packages/types_carbon__react/lib/components/UIShell/Content.d.ts
+++ packages/types_carbon__react/lib/components/UIShell/Content.d.ts
@@ -4,19 +4,21 @@ type ContentProps<K extends keyof HTMLElementTagNameMap> = {
 	/**
 	 * Provide children nodes to be rendered in the content container
 	 */
-	children?: ReactNode,
+	children?: ReactNode;
 
 	/**
 	 * Optionally provide a custom class name that is applied to the container
 	 */
-	className?: string,
+	className?: string;
 
 	/**
 	 * Optionally specify the tag of the content node. Defaults to `main`
 	 */
-	tagName?: string,
-} & HTMLProps<HTMLElementTagNameMap[K]>
+	tagName?: string;
+} & HTMLProps<HTMLElementTagNameMap[K]>;
 
-declare function Content<T extends keyof HTMLElementTagNameMap = 'main'>(props: ContentProps<T>): ReactElement;
+declare function Content<T extends keyof HTMLElementTagNameMap = 'main'>(
+	props: ContentProps<T>
+): ReactElement;
 
 export default Content;
