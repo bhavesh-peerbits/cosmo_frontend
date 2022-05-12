767fd64b50b4d0f17b6d18b9ac124dfd5ecdbdff
Stefano Imperiale <stefano.imperiale@aizoongroup.com>
2022-05-12 13:04:18 +0200

    Fix home

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -15,7 +15,7 @@ const Home = () => {
 
 			<Fade>
 				<Centered>
-					<div className='m-auto flex flex-col justify-around space-y-container-2 bg-layer-2 p-container-5'>
+					<div className='m-auto flex flex-col justify-around space-y-container-2 bg-layer-1 p-container-5'>
 						<h2 className='text-productive-heading-5'>Welcome in Cosmo</h2>
 						<p className='text-body-short-2'>
 							Navigate in the following section to start
@@ -25,9 +25,7 @@ const Home = () => {
 								<Button as={Link} to='/management'>
 									Management
 								</Button>
-								<Button kind='secondary' className='bg-layer-3'>
-									Reports
-								</Button>
+								<Button kind='secondary'>Reports</Button>
 							</ButtonSet>
 						</div>
 					</div>
