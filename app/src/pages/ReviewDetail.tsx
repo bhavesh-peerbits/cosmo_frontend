5e50a02407306de65cab2bbe6f1ae1ed7446b7fb
Federica Bruno <federica.bruno@aizoongroup.com>
2022-05-16 17:51:45 +0200

    Add TOC to ReviewDetail

--- app/src/pages/ReviewDetail.tsx
+++ app/src/pages/ReviewDetail.tsx
@@ -1,42 +1,51 @@
+import { useRef } from 'react';
+import useBreadcrumbSize from '@hooks/useBreadcrumbSize';
 import PageHeader from '@components/PageHeader';
 import GeneralInfoReview from '@components/ReviewNarrative/GeneralInfoReview';
 import { Grid, Tile } from '@carbon/react';
 import FullWidthColumn from '@components/FullWidthColumn';
+import TableOfContents from '@components/TableOfContents';
 
 const ReviewDetail = () => {
+	const { breadcrumbSize } = useBreadcrumbSize();
+	const buttonRef = useRef<HTMLDivElement>(null);
 	return (
 		<PageHeader
 			pageTitle='Name'
 			intermediateRoutes={[{ name: 'Review', to: '/review-narrative' }]}
 		>
-			<Grid fullWidth className='h-full pb-4 pr-4'>
-				<FullWidthColumn className='pt-4'>
-					<div className='space-y-7'>
-						<Tile className='w-full bg-background pb-7'>
-							<Grid>
-								<FullWidthColumn
-									data-toc-id='general-info'
-									className='flex justify-between'
-								>
-									<p className='text-productive-heading-3'>General Info</p>
-									<div>
-										<p className='text-text-secondary text-body-compact-1'>
-											Last Review Date:
+			<TableOfContents
+				stickyOffset={buttonRef.current?.getBoundingClientRect()?.height || 0}
+				tocStickyOffset={breadcrumbSize * 2}
+			>
+				<Grid fullWidth className='h-full pb-4 pr-4'>
+					<FullWidthColumn className='pt-4'>
+						<div className='space-y-7'>
+							<Tile className='w-full bg-background pb-7'>
+								<Grid>
+									<FullWidthColumn className='flex justify-between'>
+										<p data-toc-id='general-info' className='text-productive-heading-3'>
+											General Info
 										</p>
-										<p className=' text-text-secondary text-body-compact-1'>
-											Last Reviewer:
-										</p>
-									</div>
-								</FullWidthColumn>
+										<div>
+											<p className='text-text-secondary text-body-compact-1'>
+												Last Review Date:
+											</p>
+											<p className=' text-text-secondary text-body-compact-1'>
+												Last Reviewer:
+											</p>
+										</div>
+									</FullWidthColumn>
 
-								<FullWidthColumn>
-									<GeneralInfoReview />
-								</FullWidthColumn>
-							</Grid>
-						</Tile>
-					</div>
-				</FullWidthColumn>
-			</Grid>
+									<FullWidthColumn>
+										<GeneralInfoReview />
+									</FullWidthColumn>
+								</Grid>
+							</Tile>
+						</div>
+					</FullWidthColumn>
+				</Grid>
+			</TableOfContents>
 		</PageHeader>
 	);
 };
