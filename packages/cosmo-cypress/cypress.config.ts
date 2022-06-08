902ccbc3f4997d91e66e8cbc533e408a3ad10499
Stefano Imperiale <stefano.imperiale@aizoongroup.com>
2022-06-08 12:16:08 +0200

    Update cypress

---
+++ packages/cosmo-cypress/cypress.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'cypress';
+import codeCoverageTask from '@cypress/code-coverage/task';
+// import babelCoverage from '@cypress/code-coverage/use-babelrc';
+
+export default defineConfig({
+	// setupNodeEvents can be defined in either
+	// the e2e or component configuration
+	e2e: {
+		baseUrl: 'http://localhost:4173/',
+		fileServerFolder: 'dist',
+		fixturesFolder: false,
+		setupNodeEvents(on, config) {
+			void codeCoverageTask(on, config);
+			// include any other plugin code...
+
+			// It's IMPORTANT to return the config object
+			// with any changed environment variables
+			// on('file:preprocessor', babelCoverage);
+			return config;
+		}
+	}
+});
