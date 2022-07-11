a82109c2d87770ae0e7b4de0796e1dfa86a1707e
Federica Bruno <federica.bruno@aizoongroup.com>
2022-07-11 10:50:23 +0200

    Add user admin translations

--- app/src/components/AdminPanel/UserAdmin.tsx
+++ app/src/components/AdminPanel/UserAdmin.tsx
@@ -1,22 +1,28 @@
 import { Grid, Column } from '@carbon/react';
+import { useTranslation } from 'react-i18next';
 import AdminTile from './AdminTile';
 
 const UserAdmin = () => {
+	const { t } = useTranslation('userAdmin');
 	return (
 		<Grid fullWidth narrow className='mt-7 mb-7 h-full px-5'>
 			<Column sm={4} md={2} lg={4}>
-				<p className='font-bold text-productive-heading-3'>User Administration</p>
+				<p className='font-bold text-productive-heading-3'>{t('user-admin')}</p>
 				<Column>
-					<p className='mt-5 text-body-long-1'>
-						You can edit roles of users or edit applications visibility
-					</p>
+					<p className='mt-5 text-body-long-2'>{t('user-admin-description')}</p>
 				</Column>
 			</Column>
 			<Column sm={4} md={3} lg={4}>
-				<AdminTile title='Role Assignment' description='Descritpion' />
+				<AdminTile
+					title={t('role-assignment')}
+					description={t('role-assignment-description')}
+				/>
 			</Column>
 			<Column sm={4} md={3} lg={4}>
-				<AdminTile title='Application Visibility' description='Description' />
+				<AdminTile
+					title={t('app-visibility')}
+					description={t('app-visibility-description')}
+				/>
 			</Column>
 		</Grid>
 	);
---
+++ app/src/i18n/locales/components/user-admin/index.ts
@@ -0,0 +1,5 @@
+import userAdminEn from './user-admin.en.json';
+import userAdminFr from './user-admin.fr.json';
+import userAdminIt from './user-admin.it.json';
+
+export { userAdminEn, userAdminFr, userAdminIt };
