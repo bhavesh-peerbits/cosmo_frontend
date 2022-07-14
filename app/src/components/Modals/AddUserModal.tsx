c6405bbfdbd506426cbe7f26551efb0dca4fa8ef
Federica Bruno <federica.bruno@aizoongroup.com>
2022-07-14 15:50:51 +0200

    Fix layout and functionality of AddUserModal

--- app/src/components/Modals/AddUserModal.tsx
+++ app/src/components/Modals/AddUserModal.tsx
@@ -1,3 +1,4 @@
+import useGetUsers from '@api/user/useGetUsers';
 import {
 	ComposedModal,
 	ModalHeader,
@@ -7,63 +8,150 @@ import {
 	Button,
 	Column,
 	TextInput,
-	Select
+	MultiSelect,
+	Form
 } from '@carbon/react';
+import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+
+interface AddUserFormData {
+	name: string;
+	surname: string;
+	email: string;
+	username: string;
+	roles: string[];
+}
 
 type AddUserModalProps = {
 	isOpen: boolean;
 	setIsOpen: (val: boolean) => void;
 };
 const AddUserModal = ({ isOpen, setIsOpen }: AddUserModalProps) => {
+	const { data: users = [] } = useGetUsers();
 	const { t } = useTranslation('modals');
 	const { t: tHome } = useTranslation('home');
 	const { t: tUser } = useTranslation('userAdmin');
+	const existingUsername = users.map(user => user.username);
+	const existingEmail = users.map(user => user.email?.toLocaleLowerCase());
+
+	const roles = [
+		'Admin',
+		'Guest',
+		'Narrative Analyst',
+		'Reviewer',
+		'Reviewer Collaborator',
+		'User Admin'
+	];
+	const {
+		register,
+		reset,
+		formState: { isValid, errors }
+	} = useForm<AddUserFormData>({
+		mode: 'onChange',
+		defaultValues: {
+			name: '',
+			surname: '',
+			email: '',
+			username: '',
+			roles: []
+		}
+	});
+
 	const cleanUp = () => {
 		setIsOpen(false);
+		reset();
 	};
-	return (
-		<ComposedModal size='sm' open={isOpen} onClose={cleanUp}>
-			<ModalHeader title={t('edit-user')} closeModal={cleanUp}>
-				<span className='text-text-secondary text-body-1'>{t('body-edit')}</span>
-			</ModalHeader>
-			<ModalBody>
-				<Grid fullWidth>
-					<Column lg={8} md={4} sm={4} className='mb-5'>
-						<TextInput
-							id='user-name'
-							labelText={tUser('name')}
-							placeholder='Placeholder'
-						/>
-					</Column>
-					<Column lg={8} md={4} sm={4} className='mb-5'>
-						<TextInput
-							id='user-surname'
-							labelText={tUser('surname')}
-							placeholder='Placeholder'
-						/>
-					</Column>
-					<Column lg={8} md={4} sm={4} className='mb-5'>
-						<TextInput id='username' labelText='Username' placeholder='Placeholder' />
-					</Column>
-					<Column lg={8} md={4} sm={4} className='mb-5'>
-						<TextInput id='email' labelText='Email' placeholder='Placeholder' />
-					</Column>
 
-					<Column lg={8} md={4} sm={4} className='mb-5'>
-						<Select
-							id='roles'
-							labelText={tHome('roles')}
-							placeholder='Select one or more roles'
-						/>
-					</Column>
-				</Grid>
+	return (
+		<ComposedModal open={isOpen} onClose={cleanUp}>
+			<ModalHeader title={t('add-user')} closeModal={cleanUp}>
+				<span className='text-text-secondary text-body-1'>
+					{t('body-add', { action: `"${t('add-user')}"` })}
+				</span>
+			</ModalHeader>
+
+			<ModalBody hasForm>
+				<Form>
+					<Grid fullWidth className='h-full'>
+						<Column lg={8} md={4} sm={4} className='mb-5'>
+							<TextInput
+								id='user-name'
+								placeholder={tUser('placeholder-name')}
+								labelText={`${tUser('name')} *`}
+								{...register('name', {
+									required: {
+										value: true,
+										message: 'required'
+									}
+								})}
+							/>
+						</Column>
+						<Column lg={8} md={4} sm={4} className='mb-5'>
+							<TextInput
+								id='user-surname'
+								placeholder={tUser('placeholder-surname')}
+								labelText={`${tUser('surname')} *`}
+								{...register('surname', {
+									required: {
+										value: true,
+										message: 'required'
+									}
+								})}
+							/>
+						</Column>
+						<Column lg={8} md={4} sm={4} className='mb-5'>
+							<TextInput
+								id='username'
+								labelText='Username *'
+								placeholder='Username'
+								invalid={Boolean(errors.username)}
+								invalidText={errors.username?.message}
+								{...register('username', {
+									required: {
+										value: true,
+										message: `required`
+									},
+									validate: username =>
+										!existingUsername.includes(username.toLowerCase()) || 'exists'
+								})}
+							/>
+						</Column>
+						<Column lg={8} md={4} sm={4} className='mb-5'>
+							<TextInput
+								id='email'
+								labelText='Email *'
+								placeholder='email@email.com'
+								invalid={Boolean(errors.email)}
+								invalidText={errors.email?.message}
+								{...register('email', {
+									required: {
+										value: true,
+										message: `required`
+									},
+									validate: email =>
+										!existingEmail.includes(email.toLowerCase()) || 'exist'
+								})}
+							/>
+						</Column>
+						<Column lg={8} md={4} sm={4} className='mb-5 h-full'>
+							<MultiSelect
+								id='roles'
+								titleText={tHome('roles')}
+								label={tUser('select-roles')}
+								items={roles}
+								itemToString={item => item} // TODO fix layout
+							/>
+						</Column>
+					</Grid>
+				</Form>
 			</ModalBody>
 			<ModalFooter>
 				<Button kind='secondary' onClick={cleanUp}>
 					{t('cancel')}
 				</Button>
-				<Button kind='primary'>{t('add-user')}</Button>
+				<Button kind='primary' disabled={!isValid}>
+					{t('add-user')}
+				</Button>
 			</ModalFooter>
 		</ComposedModal>
 	);
