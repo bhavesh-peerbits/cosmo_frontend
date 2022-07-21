8a193454a61afb75e7b9a0857e51a296a56970b1
Federica Bruno <federica.bruno@aizoongroup.com>
2022-07-21 17:39:31 +0200

    Add content to new revalidation detail page

--- app/src/pages/NewRevalidationDetail.tsx
+++ app/src/pages/NewRevalidationDetail.tsx
@@ -1,15 +1,22 @@
 import PageHeader from '@components/PageHeader';
-import { Email, TrashCan } from '@carbon/react/icons';
+import { Email, TrashCan, Upload } from '@carbon/react/icons';
 import { useTranslation } from 'react-i18next';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import SendRevalidationModal from '@components/Modals/SendRevalidationModal';
 import DeleteCampaignModal from '@components/Modals/DeleteCampaignModal';
+import NoDataMessage from '@components/NoDataMessage';
+import { Grid, Button } from '@carbon/react';
+import FullWidthColumn from '@components/FullWidthColumn';
+import TableOfContents from '@components/TableOfContents';
+import useBreadcrumbSize from '@hooks/useBreadcrumbSize';
 
 const NewRevalidationDetail = () => {
 	const { t } = useTranslation('userRevalidation');
 	const { t: tModals } = useTranslation('modals');
 	const [isSendModalOpen, setIsSendModalOpen] = useState(false);
 	const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+	const buttonRef = useRef<HTMLDivElement>(null);
+	const { breadcrumbSize } = useBreadcrumbSize();
 	return (
 		<PageHeader
 			pageTitle='Campagna'
@@ -39,7 +46,37 @@ const NewRevalidationDetail = () => {
 					isOpen={isDeleteModalOpen}
 					setIsOpen={setIsDeleteModalOpen}
 				/>
-				<div>Content</div>
+				<TableOfContents
+					stickyOffset={buttonRef.current?.getBoundingClientRect()?.height || 0}
+					tocStickyOffset={breadcrumbSize * 2}
+				>
+					<Grid fullWidth className='h-full'>
+						<FullWidthColumn className='pt-4'>
+							<div className='space-y-4'>
+								<div
+									className='flex w-full flex-wrap items-center md:space-x-4'
+									ref={buttonRef}
+								>
+									<Button
+										size='md'
+										kind='tertiary'
+										renderIcon={Upload}
+										className='md:max-w-auto w-full max-w-full md:w-auto'
+									>
+										Upload file
+									</Button>
+								</div>
+								<div className='space-y-7'>
+									<NoDataMessage
+										className='mt-10 p-5'
+										title={t('no-upload')}
+										subtitle={t('click-to-upload')}
+									/>
+								</div>
+							</div>
+						</FullWidthColumn>
+					</Grid>
+				</TableOfContents>
 			</>
 		</PageHeader>
 	);
