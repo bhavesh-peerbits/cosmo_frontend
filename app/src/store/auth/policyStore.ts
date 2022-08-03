0731f37c79194d9319dbe37bd22a765b80f32dfe
Stefano Imperiale <stefano.imperiale@aizoongroup.com>
2022-08-03 14:34:03 +0200

    Add sysadmin as super user

--- app/src/store/auth/policyStore.ts
+++ app/src/store/auth/policyStore.ts
@@ -2,6 +2,17 @@ import { selector } from 'recoil';
 import authStore from '@store/auth/authStore';
 import { UserRoleEnum } from '@model/UserRole';
 
+declare global {
+	interface Array<T> {
+		includesMulti(...values: T[]): boolean;
+	}
+}
+function multi<T>(this: T[], ...values: T[]) {
+	return this.some(v => values.includes(v));
+}
+// eslint-disable-next-line no-extend-native
+Array.prototype.includesMulti = multi;
+
 type Policies = {
 	hasNoRole: boolean;
 	canSeeNarrativeManagement: boolean;
@@ -19,15 +30,20 @@ const policyStore = selector<Policies>({
 		return {
 			hasNoRole,
 			canSeeNarrativeManagement: Boolean(
-				!hasNoRole && policies?.includes(UserRoleEnum.NarrativeAnalyst)
+				!hasNoRole &&
+					policies?.includesMulti(UserRoleEnum.SysAdmin, UserRoleEnum.NarrativeAnalyst)
 			),
 			canReviewNarrative: Boolean(
-				!hasNoRole && policies?.includes(UserRoleEnum.NarrativeAnalyst)
+				!hasNoRole &&
+					policies?.includesMulti(UserRoleEnum.SysAdmin, UserRoleEnum.NarrativeAnalyst)
 			),
 			canReview: Boolean(
 				!hasNoRole &&
-					(policies?.includes(UserRoleEnum.Reviewer) ||
-						policies?.includes(UserRoleEnum.ReviewerCollaborator))
+					policies?.includesMulti(
+						UserRoleEnum.SysAdmin,
+						UserRoleEnum.Reviewer,
+						UserRoleEnum.ReviewerCollaborator
+					)
 			)
 		};
 	}
