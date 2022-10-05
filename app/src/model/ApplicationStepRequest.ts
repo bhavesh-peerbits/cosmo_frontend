8dd51d97b487638685fd2d383e002d8b06cb7612
Federica Bruno <federica.bruno@aizoongroup.com>
2022-10-05 14:20:31 +0200

    Add selected property to Step Request model

--- app/src/model/ApplicationStepRequest.ts
+++ app/src/model/ApplicationStepRequest.ts
@@ -3,20 +3,24 @@ import Application, { fromApplicationApi } from './Application';
 import EvidenceRequestStep, { fromEvidenceRequestStepApi } from './EvidenceRequestStep';
 
 interface ApplicationStepRequest {
+	id: string;
 	application?: Application;
 	steps?: EvidenceRequestStep[];
+	selected?: boolean;
 }
 
 export const fromApplicationStepRequestApi = (
 	applicationStepRequest: ApplicationStepRequestApi
 ): ApplicationStepRequest => {
 	return {
+		id: `${applicationStepRequest.id}`,
 		application: applicationStepRequest.application
 			? fromApplicationApi(applicationStepRequest.application)
 			: undefined,
 		steps: applicationStepRequest.steps
 			? [...applicationStepRequest.steps].map(step => fromEvidenceRequestStepApi(step))
-			: []
+			: [],
+		selected: applicationStepRequest.selected
 	};
 };
 
