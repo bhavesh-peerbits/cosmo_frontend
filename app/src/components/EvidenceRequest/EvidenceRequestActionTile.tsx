feab3055447247f0c990f6fe948228cc2577aa1a
Federica Bruno <federica.bruno@aizoongroup.com>
2022-11-02 17:34:54 +0100

    Fix tiles layout for multiple line text

--- app/src/components/EvidenceRequest/EvidenceRequestActionTile.tsx
+++ app/src/components/EvidenceRequest/EvidenceRequestActionTile.tsx
@@ -18,20 +18,20 @@ const EvidenceRequestActionTile = ({ request }: ERATileProp) => {
 					<p className='block truncate line-clamp-1 text-heading-2'>{request?.name}</p>
 					<span className='mt-2 flex space-x-2 '>
 						<p className='text-text-secondary text-body-short-1 '>{t('request-type')}:</p>
-						<p className='text-body-short-1'>{request?.type}</p>
+						<p className='block truncate text-body-short-1'>{request?.type}</p>
 					</span>
-					<span className='mt-2 flex  space-x-2 '>
-						<p className='text-text-secondary text-body-short-1 '>
+					<span className='mt-2 flex space-x-2'>
+						<p className='overflow-visible whitespace-nowrap text-text-secondary text-body-short-1'>
 							{t('workflow-type')}:
 						</p>
 						<p className='block truncate text-body-short-1'>{request.workflowType}</p>
 					</span>
 					<div>
-						<span className='mt-2 flex  space-x-2 '>
-							<p className='text-text-secondary text-body-short-1 '>
-								{t('application')}:
+						<span className='mt-2 flex space-x-2'>
+							<p className='text-text-secondary text-body-short-1'>{t('application')}:</p>
+							<p className='block truncate text-body-short-1'>
+								{request.application.name}
 							</p>
-							<p className='text-body-short-1'>{request.application.codeName}</p>
 						</span>
 					</div>
 					<div>
