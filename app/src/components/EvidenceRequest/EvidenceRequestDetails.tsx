a712a7a1ae16ce9e64170622a76350fa15ce29f0
Federica Bruno <federica.bruno@aizoongroup.com>
2022-11-14 17:18:32 +0100

    Fix evidenceRequestDetails for app long name

--- app/src/components/EvidenceRequest/EvidenceRequestDetails.tsx
+++ app/src/components/EvidenceRequest/EvidenceRequestDetails.tsx
@@ -10,7 +10,9 @@ const EvidenceRequestDetails = ({ request }: { request: EvidenceRequest }) => {
 			<p className='mt-5 font-bold text-productive-heading-2'>Framework</p>
 			<p className='text-sm text-body-compact-1'>{request.type}</p>
 			<p className='mt-5 font-bold text-productive-heading-2'>{t('application')}</p>
-			<p className='text-sm text-body-compact-1'>{request.application?.codeName}</p>
+			<p className='text-sm break-words text-body-compact-1'>
+				{request.application?.codeName}
+			</p>
 			<p className='mt-5 font-bold text-productive-heading-2'>{t('due-date')}</p>
 			<p className='text-sm text-body-compact-1'>
 				{request.dueDate.toLocaleDateString()}
