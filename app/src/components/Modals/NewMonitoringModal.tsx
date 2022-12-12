e2c6b8ae824bb7085f4648b7889a5c98a43dbb4a
Federica Bruno <federica.bruno@aizoongroup.com>
2022-12-12 15:27:31 +0100

    Add content to copy from another monitoring

--- app/src/components/Modals/NewMonitoringModal.tsx
+++ app/src/components/Modals/NewMonitoringModal.tsx
@@ -1,6 +1,23 @@
 import TearsheetNarrow from '@components/Tearsheet/TearsheetNarrow';
-import { Dispatch, SetStateAction } from 'react';
-import { RadioButton, RadioButtonGroup, Form, Toggle, TextInput } from '@carbon/react';
+import { Dispatch, SetStateAction, useState } from 'react';
+import {
+	RadioButton,
+	RadioButtonGroup,
+	Form,
+	Toggle,
+	TextInput,
+	Accordion,
+	AccordionItem,
+	Search,
+	OverflowMenu,
+	Tabs,
+	TabList,
+	Tab,
+	TabPanels,
+	TabPanel,
+	Tag
+} from '@carbon/react';
+import { Filter } from '@carbon/react/icons';
 import { useTranslation } from 'react-i18next';
 
 type NewMonitoringModalProps = {
@@ -8,7 +25,8 @@ type NewMonitoringModalProps = {
 	setIsOpen: Dispatch<SetStateAction<boolean>>;
 };
 const NewMonitoringModal = ({ isOpen, setIsOpen }: NewMonitoringModalProps) => {
-	const { t } = useTranslation('changeMonitoring');
+	const { t } = useTranslation(['changeMonitoring', 'management']);
+	const [isCopySelected, setIsCopySelected] = useState(false);
 	const cleanUp = () => {
 		setIsOpen(false);
 	};
@@ -22,25 +40,75 @@ const NewMonitoringModal = ({ isOpen, setIsOpen }: NewMonitoringModalProps) => {
 			<Form className='space-y-5 px-5'>
 				<RadioButtonGroup
 					name='monitoring-type'
-					legendText={`${t('monitoring-type')} *`}
+					legendText={`${t('changeMonitoring:monitoring-type')} *`}
 					defaultSelected='manual'
 					valueSelected='manual'
 				>
-					<RadioButton labelText={t('manual')} value='manual' />
-					<RadioButton labelText={t('automatic')} value='automatic' disabled />
+					<RadioButton labelText={t('changeMonitoring:manual')} value='manual' />
+					<RadioButton
+						labelText={t('changeMonitoring:automatic')}
+						value='automatic'
+						disabled
+					/>
 				</RadioButtonGroup>
 				<TextInput
 					id='monitoring-name'
-					labelText={`${t('monitoring-name')} *`}
-					placeholder={t('monitoring-name-placeholder')}
+					labelText={`${t('changeMonitoring:monitoring-name')} *`}
+					placeholder={t('changeMonitoring:monitoring-name-placeholder')}
 				/>
 				<Toggle
 					id='copy-monitoring-toggle'
-					labelText={t('copy-monitoring')}
+					labelText={t('changeMonitoring:copy-monitoring')}
 					labelA='No'
-					labelB={t('copy')}
+					labelB={t('changeMonitoring:copy')}
 					aria-label='Copy monitoring type'
+					onToggle={() => setIsCopySelected(!isCopySelected)}
 				/>
+				{isCopySelected && (
+					<div className='mt-3 space-y-5'>
+						<div className='flex'>
+							<Search
+								labelText=''
+								placeholder={t('changeMonitoring:search-monitoring-name')}
+							/>
+							<OverflowMenu
+								ariaLabel='monitoring-filters-menu'
+								iconDescription='Monitoring filters menu'
+								renderIcon={Filter}
+								flipped
+							>
+								<Tabs>
+									<TabList aria-label='List of tabs'>
+										<Tab>{t('management:applications')}</Tab>
+										<Tab>{t('changeMonitoring:controls')}</Tab>
+									</TabList>
+									<TabPanels>
+										<TabPanel>Applications list</TabPanel>
+										<TabPanel>Controls List</TabPanel>
+									</TabPanels>
+								</Tabs>
+							</OverflowMenu>
+						</div>
+						<Tag filter>Filters:</Tag>
+						<Accordion>
+							<AccordionItem
+								title={
+									// eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+									<div className='flex justify-start' onClick={e => e.stopPropagation()}>
+										<RadioButton
+											labelText={
+												<span className='text-productive-heading-2'>Monitoring Name</span>
+											}
+											value='value'
+										/>
+									</div>
+								}
+							>
+								cc
+							</AccordionItem>
+						</Accordion>
+					</div>
+				)}
 			</Form>
 		</TearsheetNarrow>
 	);
