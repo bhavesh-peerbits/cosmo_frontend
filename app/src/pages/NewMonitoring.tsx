2404409dbe660569e4bb20085ea7219882c3a9c0
Federica Bruno <federica.bruno@aizoongroup.com>
2022-12-12 12:01:34 +0100

    Add tile and search bar to new monitoring page

--- app/src/pages/NewMonitoring.tsx
+++ app/src/pages/NewMonitoring.tsx
@@ -1,7 +1,18 @@
 import { Add } from '@carbon/react/icons';
+import { Column, Grid, Layer, Search } from '@carbon/react';
+import MonitoringDraftTileContainer from '@components/NewMonitoring/MonitoringDraftTileContainer';
 import PageHeader from '@components/PageHeader';
 import { useTranslation } from 'react-i18next';
 
+const SearchBar = () => {
+	const { t } = useTranslation('changeMonitoring');
+
+	return (
+		<Layer className='w-full'>
+			<Search size='lg' labelText='' placeholder={t('monitoring-search-placeholder')} />
+		</Layer>
+	);
+};
 const NewMonitoring = () => {
 	const { t } = useTranslation('changeMonitoring');
 	return (
@@ -9,7 +20,30 @@ const NewMonitoring = () => {
 			pageTitle='New Monitoring'
 			actions={[{ name: t('new-monitoring'), icon: Add, onClick: () => {} }]}
 		>
-			<div>content</div>
+			<Grid fullWidth className='h-full p-container-1'>
+				<Column sm={4} md={8} lg={16}>
+					<div className='flex flex-col space-y-5'>
+						<div className='flex w-full items-center space-x-5'>
+							<SearchBar />
+							<p className='lg:whitespace-nowrap'>5 {t('drafts')}</p>
+							{/* // TODO fix when be is ready */}
+						</div>
+						<div>
+							{/* {drafts.length === 0 ? (
+								<Fade>
+									<Centered>
+										<NoDataMessage title={t('no-drafts')} />
+									</Centered>
+								</Fade>
+							) : (
+								<MonitoringDraftTileContainer />
+							)} */}
+							{/* // TODO to implement when BE is ready */}
+							<MonitoringDraftTileContainer />
+						</div>
+					</div>
+				</Column>
+			</Grid>
 		</PageHeader>
 	);
 };
