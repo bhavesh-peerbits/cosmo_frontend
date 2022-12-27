269bffd4ac61199ec0f049f6f6417b06966550ac
Federica Bruno <federica.bruno@aizoongroup.com>
2022-12-27 12:29:51 +0100

    Add close monitoring modal

---
+++ app/src/components/Modals/CloseMonitoringModal.tsx
@@ -0,0 +1,56 @@
+import {
+	Button,
+	ComposedModal,
+	ModalBody,
+	ModalFooter,
+	ModalHeader
+} from '@carbon/react';
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { Dispatch, SetStateAction } from 'react';
+
+type CloseMonitoringProps = {
+	isOpen: boolean;
+	setIsOpen: Dispatch<SetStateAction<string | undefined>>;
+	id: string;
+};
+
+const CloseMonitoringModal = ({ isOpen, setIsOpen, id }: CloseMonitoringProps) => {
+	const navigate = useNavigate();
+	const { t } = useTranslation(['modals', 'monitoringDashboard']);
+
+	const cleanUp = () => {
+		setIsOpen('');
+	};
+
+	return (
+		<ComposedModal size='xs' open={isOpen} onClose={cleanUp}>
+			<ModalHeader title={t('monitoringDashboard:confirm-close')} closeModal={cleanUp} />
+			<ModalBody>
+				<span>{`${t('monitoringDashboard:close-monitoring', { name: id })}`}</span>
+				{/* {isError && (
+					<div className='mt-5 flex items-center justify-center'>
+						<InlineNotification
+							kind='error'
+							title='Error'
+							hideCloseButton
+							subtitle={
+								(error as ApiError)?.message ||
+								'An error has occurred, please try again later'
+							}
+						/>
+					</div>
+				)} */}
+			</ModalBody>
+			<ModalFooter>
+				<Button kind='secondary' onClick={cleanUp}>
+					{t('modals:cancel')}
+				</Button>
+				<Button kind='danger' onClick={() => navigate('monitoring-dashboard')}>
+					{t('modals:close')}
+				</Button>
+			</ModalFooter>
+		</ComposedModal>
+	);
+};
+export default CloseMonitoringModal;
--- app/src/pages/StartedMonitoringDetails.tsx
+++ app/src/pages/StartedMonitoringDetails.tsx
@@ -1,19 +1,34 @@
 import PageHeader from '@components/PageHeader';
 import { CloseOutline, Collaborate, Group } from '@carbon/react/icons';
 import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
+import CloseMonitoringModal from '@components/Modals/CloseMonitoringModal';
+import { useParams } from 'react-router-dom';
 
 const StartedMonitoringDetails = () => {
 	const { t } = useTranslation('evidenceRequest');
+	const { monitoringId = '' } = useParams();
+	const [modalOpen, setModalOpen] = useState<string>();
 	return (
 		<PageHeader
 			pageTitle='Monitoring Name'
 			actions={[
 				{ name: t('collaborators'), onClick: () => {}, icon: Collaborate },
 				{ name: 'Focal Point', onClick: () => {}, icon: Group },
-				{ name: t('close'), onClick: () => {}, icon: CloseOutline }
+				{
+					name: t('close'),
+					onClick: () => {
+						setModalOpen('close');
+					},
+					icon: CloseOutline
+				}
 			]}
 		>
-			<div>content</div>
+			<CloseMonitoringModal
+				isOpen={!!modalOpen}
+				setIsOpen={setModalOpen}
+				id={monitoringId}
+			/>
 		</PageHeader>
 	);
 };
