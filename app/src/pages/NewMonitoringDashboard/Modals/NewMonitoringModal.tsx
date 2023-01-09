0fbcc82475d2a69040376b9354284740045d24f2
Federica Bruno <federica.bruno@aizoongroup.com>
2023-01-09 16:53:03 +0100

    Add errors notification to new monitoring modal

--- app/src/pages/NewMonitoringDashboard/Modals/NewMonitoringModal.tsx
+++ app/src/pages/NewMonitoringDashboard/Modals/NewMonitoringModal.tsx
@@ -9,7 +9,8 @@ import {
 	Accordion,
 	AccordionItem,
 	Search,
-	Tag
+	Tag,
+	InlineNotification
 } from '@carbon/react';
 import { useTranslation } from 'react-i18next';
 import CosmoFiltersPanel from '@components/CosmoFiltersPanel';
@@ -18,6 +19,7 @@ import useCreateDraftMonitoring from '@api/change-monitoring/useCreateMonitoring
 import { useForm } from 'react-hook-form';
 import useGetAllMonitoringDraftNames from '@api/change-monitoring/useGetAllMonitoringDraftNames';
 import { useNavigate } from 'react-router-dom';
+import ApiError from '@api/ApiError';
 import NewMonitoringFilters from '../Components/NewMonitoringFilters';
 
 type NewMonitoringForm = {
@@ -46,7 +48,7 @@ const NewMonitoringModal = ({ isOpen, setIsOpen }: NewMonitoringModalProps) => {
 	} = useForm<NewMonitoringForm>({ mode: 'onChange' });
 
 	const navigate = useNavigate();
-	const { mutate } = useCreateDraftMonitoring();
+	const { mutate, error, isError } = useCreateDraftMonitoring();
 	const { data: draftNames } = useGetAllMonitoringDraftNames();
 
 	const [isCopySelected, setIsCopySelected] = useState(false);
@@ -99,133 +101,148 @@ const NewMonitoringModal = ({ isOpen, setIsOpen }: NewMonitoringModalProps) => {
 				}
 			]}
 		>
-			<Form className='space-y-5 px-5'>
-				<RadioButtonGroup
-					name='monitoring-type'
-					legendText={`${t('changeMonitoring:monitoring-type')} *`}
-					defaultSelected='manual'
-					valueSelected={getValues('type')}
-					onChange={value => setValue('type', value.toString())}
-				>
-					<RadioButton labelText={t('changeMonitoring:manual')} value='manual' />
-					<RadioButton
-						labelText={t('changeMonitoring:automatic')}
-						value='automatic'
-						disabled
+			<>
+				<Form className='space-y-5 px-5'>
+					<RadioButtonGroup
+						name='monitoring-type'
+						legendText={`${t('changeMonitoring:monitoring-type')} *`}
+						defaultSelected='manual'
+						valueSelected={getValues('type')}
+						onChange={value => setValue('type', value.toString())}
+					>
+						<RadioButton labelText={t('changeMonitoring:manual')} value='manual' />
+						<RadioButton
+							labelText={t('changeMonitoring:automatic')}
+							value='automatic'
+							disabled
+						/>
+					</RadioButtonGroup>
+					<TextInput
+						id='monitoring-name'
+						labelText={`${t('changeMonitoring:monitoring-name')} *`}
+						placeholder={t('changeMonitoring:monitoring-name-placeholder')}
+						invalidText={errors.name?.message}
+						invalid={Boolean(errors.name)}
+						{...register('name', {
+							required: {
+								value: true,
+								message: `${t('modals:field-required')}`
+							},
+							validate: name =>
+								!draftNames
+									?.map(existingName => existingName.toLowerCase())
+									.includes(name.toLowerCase()) || t('applicationInfo:name-exists')
+						})}
 					/>
-				</RadioButtonGroup>
-				<TextInput
-					id='monitoring-name'
-					labelText={`${t('changeMonitoring:monitoring-name')} *`}
-					placeholder={t('changeMonitoring:monitoring-name-placeholder')}
-					invalidText={errors.name?.message}
-					invalid={Boolean(errors.name)}
-					{...register('name', {
-						required: {
-							value: true,
-							message: `${t('modals:field-required')}`
-						},
-						validate: name =>
-							!draftNames
-								?.map(existingName => existingName.toLowerCase())
-								.includes(name.toLowerCase()) || t('applicationInfo:name-exists')
-					})}
-				/>
-				<Toggle
-					id='copy-monitoring-toggle'
-					labelText={t('changeMonitoring:copy-monitoring')}
-					labelA='No'
-					labelB={t('changeMonitoring:copy')}
-					aria-label='Copy monitoring type'
-					toggled={isCopySelected}
-					onToggle={() => setIsCopySelected(!isCopySelected)}
-				/>
-				{isCopySelected && (
-					<div className='mt-3 space-y-5'>
-						<div className='flex'>
-							<Search
-								labelText=''
-								placeholder={t('changeMonitoring:search-monitoring-name')}
-							/>
-							<CosmoFiltersPanel>
-								<NewMonitoringFilters
-									setSelectedItems={setSelectedItemsFilters}
-									selectedItems={selectedItemsFilters}
+					<Toggle
+						id='copy-monitoring-toggle'
+						labelText={t('changeMonitoring:copy-monitoring')}
+						labelA='No'
+						labelB={t('changeMonitoring:copy')}
+						aria-label='Copy monitoring type'
+						toggled={isCopySelected}
+						onToggle={() => setIsCopySelected(!isCopySelected)}
+					/>
+					{isCopySelected && (
+						<div className='mt-3 space-y-5'>
+							<div className='flex'>
+								<Search
+									labelText=''
+									placeholder={t('changeMonitoring:search-monitoring-name')}
 								/>
-							</CosmoFiltersPanel>
-						</div>
-						{(selectedItemsFilters.applications.length > 0 ||
-							selectedItemsFilters.controls.length > 0) && (
-							<div className='space-x-3'>
-								<span className='text-heading-1'>{t('management:filters')}:</span>
-								{selectedItemsFilters.applications.length > 0 && (
-									<Tag
-										filter
-										onClose={() =>
-											setSelectedItemsFilters(old => ({ ...old, applications: [] }))
-										}
-									>
-										{`${selectedItemsFilters.applications.length} `}
-										{t('management:applications')}
-									</Tag>
-								)}
-								{selectedItemsFilters.controls.length > 0 && (
-									<Tag
-										filter
-										onClose={() =>
-											setSelectedItemsFilters(old => ({ ...old, controls: [] }))
-										}
-									>
-										{`${selectedItemsFilters.controls.length} `}
-										{t('changeMonitoring:controls')}
-									</Tag>
-								)}
+								<CosmoFiltersPanel>
+									<NewMonitoringFilters
+										setSelectedItems={setSelectedItemsFilters}
+										selectedItems={selectedItemsFilters}
+									/>
+								</CosmoFiltersPanel>
 							</div>
-						)}
-
-						<Accordion>
-							<AccordionItem
-								key='c'
-								title={
-									// eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-									<div
-										className='flex w-fit justify-start'
-										onClick={e => e.stopPropagation()}
-									>
-										<RadioButton
-											labelText={
-												<span className='text-productive-heading-2'>Monitoring Name</span>
+							{(selectedItemsFilters.applications.length > 0 ||
+								selectedItemsFilters.controls.length > 0) && (
+								<div className='space-x-3'>
+									<span className='text-heading-1'>{t('management:filters')}:</span>
+									{selectedItemsFilters.applications.length > 0 && (
+										<Tag
+											filter
+											onClose={() =>
+												setSelectedItemsFilters(old => ({ ...old, applications: [] }))
 											}
-											value='value'
-											onChange={value => setSelectedMonitoring(value)}
-											checked={selectedMonitoring === 'value'}
-										/>
-									</div>
-								}
-							>
-								<div className='flex flex-col'>
-									<div className='flex space-x-1'>
-										<span className='text-heading-1'>
-											{t('changeMonitoring:completed-runs')}:
-										</span>
-										<span>5</span>
-									</div>
-									<div className='flex space-x-1'>
-										<span className='text-heading-1'>
-											{t('changeMonitoring:scheduling')}:
-										</span>
-										<span>On Demand</span>
-									</div>
-									<div className='mt-3 flex space-x-1'>
-										<span className='text-heading-1'>Path:</span>
-										<span>Path</span>
-									</div>
+										>
+											{`${selectedItemsFilters.applications.length} `}
+											{t('management:applications')}
+										</Tag>
+									)}
+									{selectedItemsFilters.controls.length > 0 && (
+										<Tag
+											filter
+											onClose={() =>
+												setSelectedItemsFilters(old => ({ ...old, controls: [] }))
+											}
+										>
+											{`${selectedItemsFilters.controls.length} `}
+											{t('changeMonitoring:controls')}
+										</Tag>
+									)}
 								</div>
-							</AccordionItem>
-						</Accordion>
-					</div>
+							)}
+
+							<Accordion>
+								<AccordionItem
+									key='c'
+									title={
+										// eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+										<div
+											className='flex w-fit justify-start'
+											onClick={e => e.stopPropagation()}
+										>
+											<RadioButton
+												labelText={
+													<span className='text-productive-heading-2'>
+														Monitoring Name
+													</span>
+												}
+												value='value'
+												onChange={value => setSelectedMonitoring(value)}
+												checked={selectedMonitoring === 'value'}
+											/>
+										</div>
+									}
+								>
+									<div className='flex flex-col'>
+										<div className='flex space-x-1'>
+											<span className='text-heading-1'>
+												{t('changeMonitoring:completed-runs')}:
+											</span>
+											<span>5</span>
+										</div>
+										<div className='flex space-x-1'>
+											<span className='text-heading-1'>
+												{t('changeMonitoring:scheduling')}:
+											</span>
+											<span>On Demand</span>
+										</div>
+										<div className='mt-3 flex space-x-1'>
+											<span className='text-heading-1'>Path:</span>
+											<span>Path</span>
+										</div>
+									</div>
+								</AccordionItem>
+							</Accordion>
+						</div>
+					)}
+				</Form>
+				{isError && (
+					<InlineNotification
+						kind='error'
+						title='Error'
+						hideCloseButton
+						subtitle={
+							(error as ApiError)?.message ||
+							'An error has occurred, please try again later'
+						}
+					/>
 				)}
-			</Form>
+			</>
 		</TearsheetNarrow>
 	);
 };
