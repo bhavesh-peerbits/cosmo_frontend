4b3587eed6a42542b68d385afaf597d388178e73
Federica Bruno <federica.bruno@aizoongroup.com>
2023-01-23 17:52:15 +0100

    Fix inbox monitoring filters

--- app/src/pages/ChangeMonitoringInbox/Components/InboxMonitoringFilters.tsx
+++ app/src/pages/ChangeMonitoringInbox/Components/InboxMonitoringFilters.tsx
@@ -1,251 +1,150 @@
-import {
-	Accordion,
-	AccordionItem,
-	Checkbox,
-	Layer,
-	DatePickerInput,
-	DatePicker,
-	Button,
-	Form,
-	TextInput
-} from '@carbon/react';
-import { Reset } from '@carbon/react/icons';
+import { Checkbox, Layer, Button, TextInput, Form } from '@carbon/react';
 import { useTranslation } from 'react-i18next';
-import useResponsive from '@hooks/useResponsive';
+import CosmoFiltersPanel from '@components/CosmoFiltersPanel';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import DatePickerWrapper from '@components/DatePickerWrapper';
+import { FrequencyFrequencyTypesEnum } from 'cosmo-api/src/v1';
 import useInboxMonitorings from '@hooks/inbox-monitoring/useInboxMonitorings';
 
+type MonitoringFiltersForm = {
+	minStartDate: Date;
+	maxStartDate: Date;
+	minEndDate: Date;
+	maxEndDate: Date;
+	currentRun: string;
+	totalRuns: string;
+};
 const InboxMonitoringFilters = () => {
 	const { t } = useTranslation([
 		'changeMonitoring',
 		'monitoringDashboard',
-		'evidenceRequest'
+		'evidenceRequest',
+		'userSelect'
 	]);
-	const { filtersAvailable, setFilters, filters } = useInboxMonitorings();
-	const { md } = useResponsive();
+	const { filtersAvailable, setFilters } = useInboxMonitorings();
 
-	const handleCheckFilter = (filter: string, action: 'add' | 'remove') => {
-		setFilters(old => ({
-			frequency:
-				action === 'add'
-					? [...(old.frequency ?? []), filter]
-					: (old.frequency ?? []).filter((f: string) => f !== filter)
-		}));
-	};
+	const { register, control, reset, handleSubmit } = useForm<MonitoringFiltersForm>({
+		defaultValues: {
+			minStartDate: undefined,
+			maxStartDate: undefined,
+			minEndDate: undefined,
+			maxEndDate: undefined,
+			currentRun: undefined,
+			totalRuns: undefined
+		}
+	});
+	const [frequencyChecked, setFrequencyChecked] = useState(['']);
 
-	const handleDateFilter = (
-		filter: string,
-		property: 'minStartDate' | 'maxStartDate' | 'minEndDate' | 'maxEndDate'
-	) => {
-		setFilters(old => ({
-			...old,
-			[property]: filter
-		}));
+	const saveFilters = (data: MonitoringFiltersForm) => {
+		setFilters({
+			frequency: frequencyChecked || [],
+			minStartDate: data.minStartDate ? data.minStartDate.toDateString() : undefined,
+			maxStartDate: data.maxStartDate ? data.maxStartDate.toDateString() : undefined,
+			minEndDate: data.minEndDate ? data.minEndDate.toDateString() : undefined,
+			maxEndDate: data.maxEndDate ? data.maxEndDate.toDateString() : undefined,
+			currentRun: +data.currentRun || undefined,
+			numberOfRun: +data.totalRuns || undefined
+		});
 	};
 
 	return (
-		<div className='flex flex-col'>
-			<Accordion className='divide-y'>
-				<AccordionItem
-					title={t('changeMonitoring:start-date')}
-					className='border-0'
-					open={md}
-				>
-					<Layer>
-						<Form className='space-y-5'>
-							<DatePicker
-								id='date-picker'
-								datePickerType='single'
-								dateFormat='d/m'
-								onChange={e => handleDateFilter(e[0].toDateString(), 'minStartDate')}
-								maxDate={
-									filters.maxStartDate
-										? new Date(filters.maxStartDate).toLocaleDateString()
-										: new Date('1/1/3000')
-								}
-								value={
-									filters.minStartDate
-										? new Date(filters.minStartDate).toLocaleDateString().slice(0, -5)
-										: null
-								}
-							>
-								<DatePickerInput
-									labelText={t('evidenceRequest:before')}
-									id='min'
-									size='sm'
-									autoComplete='off'
-									placeholder='dd/mm'
-								/>
-							</DatePicker>
-							<DatePicker
-								id='date-picker'
-								datePickerType='single'
-								dateFormat='d/m'
-								onChange={e => handleDateFilter(e[0]?.toDateString(), 'maxStartDate')}
-								minDate={
-									filters.minStartDate
-										? new Date(filters.minStartDate).toLocaleDateString()
-										: new Date(0)
-								}
-								value={
-									filters.maxStartDate
-										? new Date(filters.maxStartDate).toLocaleDateString().slice(0, -5)
-										: null
-								}
-							>
-								<DatePickerInput
-									labelText={t('evidenceRequest:after')}
-									id='max'
-									size='sm'
-									autoComplete='off'
-									placeholder='dd/mm'
-								/>
-							</DatePicker>
-
-							<Button
-								type='reset'
-								kind='tertiary'
-								className='mt-3 w-full max-w-[212px]'
-								size='sm'
-								renderIcon={Reset}
-								onClick={() => {
-									setFilters(() => ({
-										maxStartDate: undefined,
-										minStartDate: undefined
-									}));
-								}}
-							>
-								Reset
-							</Button>
-						</Form>
-					</Layer>
-				</AccordionItem>
-				<AccordionItem
-					title={t('changeMonitoring:end-date')}
-					className='border-0'
-					open={md}
-				>
-					<Layer>
-						<Form className='space-y-5'>
-							<DatePicker
-								id='date-picker'
-								datePickerType='single'
-								dateFormat='d/m'
-								onChange={e => handleDateFilter(e[0].toDateString(), 'minStartDate')}
-								maxDate={
-									filters.maxEndDate
-										? new Date(filters.maxEndDate).toLocaleDateString()
-										: new Date('1/1/3000')
-								}
-								value={
-									filters.minEndDate
-										? new Date(filters.minEndDate).toLocaleDateString().slice(0, -5)
-										: null
-								}
-							>
-								<DatePickerInput
-									labelText={t('evidenceRequest:before')}
-									id='min'
-									size='sm'
-									autoComplete='off'
-									placeholder='dd/mm'
-								/>
-							</DatePicker>
-							<DatePicker
-								id='date-picker'
-								datePickerType='single'
-								dateFormat='d/m'
-								onChange={e => handleDateFilter(e[0]?.toDateString(), 'maxEndDate')}
-								minDate={
-									filters.minEndDate
-										? new Date(filters.minEndDate).toLocaleDateString()
-										: new Date(0)
-								}
-								value={
-									filters.maxEndDate
-										? new Date(filters.maxEndDate).toLocaleDateString().slice(0, -5)
-										: null
-								}
-							>
-								<DatePickerInput
-									labelText={t('evidenceRequest:after')}
-									id='max'
-									size='sm'
-									autoComplete='off'
-									placeholder='dd/mm'
-								/>
-							</DatePicker>
-
-							<Button
-								type='reset'
-								kind='tertiary'
-								className='mt-3 w-full max-w-[212px]'
-								size='sm'
-								renderIcon={Reset}
-								onClick={() => {
-									setFilters(() => ({
-										maxEndDate: undefined,
-										minEndDate: undefined
-									}));
-								}}
-							>
-								Reset
-							</Button>
-						</Form>
-					</Layer>
-				</AccordionItem>
-				<AccordionItem
-					title={t('changeMonitoring:frequency')}
-					className='border-0'
-					open={md}
-				>
-					<Checkbox
-						labelText={t('monitoringDashboard:all')}
-						id='owner-all'
-						checked={filtersAvailable.frequency.every(f => f.enabled)}
-						onChange={(_, { checked }) =>
-							setFilters({
-								frequency: checked
-									? filtersAvailable.frequency.map(({ frequency }) => frequency)
-									: []
-							})
-						}
+		<CosmoFiltersPanel flipped>
+			<Form
+				onReset={() => {
+					reset();
+					setFilters({
+						frequency: [],
+						numberOfRun: undefined,
+						minStartDate: undefined,
+						maxStartDate: undefined,
+						minEndDate: undefined,
+						maxEndDate: undefined,
+						currentRun: undefined
+					});
+					setFrequencyChecked([]);
+				}}
+			>
+				<Layer className='space-y-5'>
+					<p className='text-body-short-2'>{t('userSelect:filters')}:</p>
+					<DatePickerWrapper
+						control={control}
+						name='minStartDate'
+						label={t('changeMonitoring:min-start-date')}
 					/>
-					{filtersAvailable.frequency.map(filter => (
+					<DatePickerWrapper
+						control={control}
+						name='maxStartDate'
+						label={t('changeMonitoring:max-start-date')}
+					/>
+					<DatePickerWrapper
+						control={control}
+						name='minEndDate'
+						label={t('changeMonitoring:min-end-date')}
+					/>
+					<DatePickerWrapper
+						control={control}
+						name='maxEndDate'
+						label={t('changeMonitoring:max-end-date')}
+					/>
+					<div className='space-y-3'>
+						<p className='text-text-secondary text-label-1'>
+							{t('changeMonitoring:frequency')}
+						</p>
 						<Checkbox
-							key={filter.frequency}
-							checked={filter.enabled}
-							onChange={(_, { checked, id }) =>
-								handleCheckFilter(id, checked ? 'add' : 'remove')
+							labelText={t('monitoringDashboard:all')}
+							id='frequency-all'
+							onChange={(_, { checked }) =>
+								setFrequencyChecked(
+									checked
+										? filtersAvailable.frequency.map(({ frequency }) => frequency)
+										: []
+								)
 							}
-							id={filter.frequency}
-							labelText={filter.frequency}
 						/>
-					))}
-				</AccordionItem>
-				<AccordionItem title={t('monitoringDashboard:current-run')}>
-					<Layer>
-						<TextInput
-							id='current-run-filter'
-							labelText={t('monitoringDashboard:current-run')}
-							hideLabel
-							onChange={e => setFilters({ currentRun: +e.currentTarget.value })}
-							placeholder={t('monitoringDashboard:number-placeholder')}
-						/>
-					</Layer>
-				</AccordionItem>
-				<AccordionItem title={t('changeMonitoring:total-runs')}>
-					<Layer>
-						<TextInput
-							id='total-runs-filter'
-							labelText={t('changeMonitoring:total-runs')}
-							hideLabel
-							onChange={e => setFilters({ numberOfRun: +e.currentTarget.value })}
-							placeholder={t('monitoringDashboard:number-placeholder')}
-						/>
-					</Layer>
-				</AccordionItem>
-			</Accordion>
-		</div>
+						{filtersAvailable.frequency.map(filter => (
+							<Checkbox
+								key={filter.frequency}
+								// checked={filter.enabled}
+								onChange={(_, { checked, id }) =>
+									setFrequencyChecked(old => (checked ? [...old, id] : old))
+								}
+								id={filter.frequency}
+								labelText={t(
+									`changeMonitoring:${filter.frequency as FrequencyFrequencyTypesEnum}`
+								)}
+							/>
+						))}
+					</div>
+					<TextInput
+						id='current-run-filter'
+						labelText={t('monitoringDashboard:current-run')}
+						placeholder={t('monitoringDashboard:number-placeholder')}
+						{...register('currentRun')}
+					/>
+					<TextInput
+						id='total-runs-filter'
+						labelText={t('changeMonitoring:total-runs')}
+						placeholder={t('monitoringDashboard:number-placeholder')}
+						{...register('totalRuns')}
+					/>
+					<div>
+						<div className='space-y-3'>
+							<div className='w-full'>
+								<Button className='w-full' size='sm' kind='secondary' type='reset'>
+									Reset
+								</Button>
+							</div>
+							<div className='w-full'>
+								<Button className='w-full' size='sm' onClick={handleSubmit(saveFilters)}>
+									{t('evidenceRequest:save')}
+								</Button>
+							</div>
+						</div>
+					</div>
+				</Layer>
+			</Form>
+		</CosmoFiltersPanel>
 	);
 };
 
