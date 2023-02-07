da7cd849945ed5c42b53a661ce1343ffdfec0c2b
giacomo.parlatoni <giacomo.parlatoni@aizoongroup.com>
2023-02-07 14:56:08 +0100

    fix tearsheet over modal

--- app/src/components/Modals/ModifyAnswerModal.tsx
+++ app/src/components/Modals/ModifyAnswerModal.tsx
@@ -1,18 +1,9 @@
 import useSaveModifiedAnswer from '@api/review-campaign/useSaveModifiedAnswer';
 import useGetUsersByRole from '@api/user/useGetUsersByRole';
-import {
-	Button,
-	ComposedModal,
-	Form,
-	ModalBody,
-	ModalFooter,
-	ModalHeader,
-	TextArea,
-	TextInput,
-	InlineLoading
-} from '@carbon/react';
+import { Form, TextArea, TextInput } from '@carbon/react';
 import MultipleUserSelect from '@components/MultipleUserSelect';
 import SingleUserSelect from '@components/SingleUserSelect';
+import TearsheetNarrow from '@components/Tearsheet/TearsheetNarrow';
 import User from '@model/User';
 import modifyAnswerModalInfo from '@store/user-revalidation/modifyAnswerModalInfo';
 import { useEffect } from 'react';
@@ -114,101 +105,99 @@ const ModifyAnswerModal = () => {
 	};
 
 	return (
-		<ComposedModal
+		<TearsheetNarrow
+			hasCloseIcon
+			title={t('userRevalidation:modify-answer')}
+			actions={[
+				{
+					label: t('modals:cancel'),
+					kind: 'secondary',
+					onClick: cleanUp,
+					id: 'cancel-modify-answer'
+				},
+				{
+					label: t('modals:save'),
+					id: 'send-focal-point',
+					disabled: !isValid || isLoading,
+					onClick: handleSubmit(modifyAnswer)
+				}
+			]}
 			open={modifyModal.open}
 			onClose={cleanUp}
-			preventCloseOnClickOutside
-			className='z-[9000]'
 		>
-			<ModalHeader title={t('userRevalidation:modify-answer')} closeModal={cleanUp} />
-			<ModalBody className='mb-3 max-h-[500px] overflow-y-auto'>
-				<Form>
-					<div className='grid grid-cols-2 gap-5 p-5'>
-						<div>
-							<SingleUserSelect
-								control={control}
-								label={t('userRevalidation:revalidators')}
-								name='revalidationUser'
-								level={2}
-								// TODO Add default value
-								getUserFn={() => {
-									// eslint-disable-next-line react-hooks/rules-of-hooks
-									return useGetUsersByRole('FOCAL_POINT');
-								}}
-								defaultValue={modifyModal.answer?.revalidationUser}
-							/>
-						</div>
-						<div>
-							<MultipleUserSelect
-								control={control}
-								label='Delegates'
-								name='delegated'
-								level={2}
-								defaultValue={modifyModal.answer?.delegated}
-								getUserFn={() => {
-									// eslint-disable-next-line react-hooks/rules-of-hooks
-									return useGetUsersByRole('FOCAL_POINT');
-								}}
-							/>
-						</div>
-						<TextInput
-							id='userToRevalidate'
-							labelText={t('userRevalidation:users-to-revalidate')}
-							{...register('userToRevalidate')}
-						/>
-						<TextInput
-							id='userDetails'
-							labelText={t('userRevalidation:user-details')}
-							{...register('userDetails')}
-						/>
-						<TextArea
-							id='permission'
-							labelText={t('userRevalidation:permission')}
-							{...register('permissions')}
-							className='col-span-2'
-						/>
-						<TextArea
-							id='permissionDescription'
-							rows={2}
-							labelText={t('userRevalidation:permission-description')}
-							className='col-span-2'
-							{...register('permissionDescription')}
-						/>
-
-						<TextInput
-							id='risk'
-							labelText={t('userRevalidation:risk')}
-							{...register('risk')}
-						/>
-						<TextInput
-							id='firefighter'
-							labelText={t('userRevalidation:firefighter')}
-							{...register('firefighterID')}
-						/>
-						<TextArea
-							id='riskDescription'
-							rows={2}
-							labelText={t('userRevalidation:risk-description')}
-							className='col-span-2'
-							{...register('riskDescription')}
+			<Form>
+				<div className='grid grid-cols-2 gap-5 p-5'>
+					<div>
+						<SingleUserSelect
+							control={control}
+							label={t('userRevalidation:revalidators')}
+							name='revalidationUser'
+							level={1}
+							// TODO Add default value
+							getUserFn={() => {
+								// eslint-disable-next-line react-hooks/rules-of-hooks
+								return useGetUsersByRole('FOCAL_POINT');
+							}}
+							defaultValue={modifyModal.answer?.revalidationUser}
 						/>
 					</div>
-				</Form>
-			</ModalBody>
-			<ModalFooter>
-				<Button kind='secondary' onClick={cleanUp}>
-					{t('modals:cancel')}
-				</Button>
-				<Button
-					kind='primary'
-					onClick={handleSubmit(modifyAnswer)}
-					disabled={!isValid || isLoading}
-				>
-					<span className='mr-5'>{t('modals:save')}</span>
-					{isLoading && <InlineLoading />}
-				</Button>
-			</ModalFooter>
-		</ComposedModal>
+					<div>
+						<MultipleUserSelect
+							control={control}
+							label='Delegates'
+							name='delegated'
+							level={2}
+							defaultValue={modifyModal.answer?.delegated}
+							getUserFn={() => {
+								// eslint-disable-next-line react-hooks/rules-of-hooks
+								return useGetUsersByRole('FOCAL_POINT');
+							}}
+						/>
+					</div>
+					<TextInput
+						id='userToRevalidate'
+						labelText={t('userRevalidation:users-to-revalidate')}
+						{...register('userToRevalidate')}
+					/>
+					<TextInput
+						id='userDetails'
+						labelText={t('userRevalidation:user-details')}
+						{...register('userDetails')}
+					/>
+					<TextArea
+						id='permission'
+						labelText={t('userRevalidation:permission')}
+						{...register('permissions')}
+						className='col-span-2'
+					/>
+					<TextArea
+						id='permissionDescription'
+						rows={2}
+						labelText={t('userRevalidation:permission-description')}
+						className='col-span-2'
+						{...register('permissionDescription')}
+					/>
+
+					<TextInput
+						id='risk'
+						labelText={t('userRevalidation:risk')}
+						{...register('risk')}
+					/>
+					<TextInput
+						id='firefighter'
+						labelText={t('userRevalidation:firefighter')}
+						{...register('firefighterID')}
+					/>
+					<TextArea
+						id='riskDescription'
+						rows={2}
+						labelText={t('userRevalidation:risk-description')}
+						className='col-span-2'
+						{...register('riskDescription')}
+					/>
+				</div>
+			</Form>
+		</TearsheetNarrow>
 	);
 };
 export default ModifyAnswerModal;
